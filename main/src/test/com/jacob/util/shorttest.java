bfbc425801c401fc6a13a6437d950e6518f705d1
messagelost <1692123221@qq.com>
2024-03-13 23:33:52 +0800

    fifth

---
+++ main/src/test/com/jacob/util/shorttest.java
@@ -0,0 +1,13 @@
+package com.jacob.util;
+
+import org.junit.Test;
+
+public class shorttest {
+
+    @Test
+    public void shortStringExceptionTest(){
+        //测试str.length()<200的情况
+        System.out.println(com.jacob.util.simhash.getSimHash("一位正真的作家"));
+    }
+
+}
